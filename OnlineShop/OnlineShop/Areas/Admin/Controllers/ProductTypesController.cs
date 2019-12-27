90b2700908fcc9e4e4eb29f482e413ef0e37f31a
Rakib Islam <rakib.shehab@gmail.com>
2019-12-27 19:14:55 +0600

    Second commit

---
+++ OnlineShop/OnlineShop/Areas/Admin/Controllers/ProductTypesController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using OnlineShop.Data;
+
+namespace OnlineShop.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class ProductTypesController : Controller
+    {
+        private ApplicationDbContext _db;
+        public ProductTypesController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public IActionResult Index()
+        {
+            return View(_db.ProductTypes.ToList());
+        }
+    }
+}
