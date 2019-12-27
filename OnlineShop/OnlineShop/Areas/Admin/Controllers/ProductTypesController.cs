6b7e52832fe569d3cf95ec7836de28164ee54b8f
Rakib Islam <rakib.shehab@gmail.com>
2019-12-27 23:31:08 +0600

    third commit

--- OnlineShop/OnlineShop/Areas/Admin/Controllers/ProductTypesController.cs
+++ OnlineShop/OnlineShop/Areas/Admin/Controllers/ProductTypesController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using OnlineShop.Data;
+using OnlineShop.Models;
 
 namespace OnlineShop.Areas.Admin.Controllers
 {
@@ -19,6 +20,22 @@ namespace OnlineShop.Areas.Admin.Controllers
         public IActionResult Index()
         {
             return View(_db.ProductTypes.ToList());
+        }
+        
+        public ActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Create(ProductTypes productTypes)
+        {
+            if(ModelState.IsValid)
+            {
+                _db.ProductTypes.Add(productTypes);
+                await _db.SaveChangesAsync();
+            }
+            return RedirectToAction(nameof(Index));
         }
     }
 }
