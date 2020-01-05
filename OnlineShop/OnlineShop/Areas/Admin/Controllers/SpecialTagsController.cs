574a59218a006cf8cc2ed6193f7d51ad2d68d20b
Rakib Islam <rakib.shehab@gmail.com>
2020-01-05 21:35:40 +0600

    comit

--- OnlineShop/OnlineShop/Areas/Admin/Controllers/SpecialTagsController.cs
+++ OnlineShop/OnlineShop/Areas/Admin/Controllers/SpecialTagsController.cs
@@ -3,14 +3,21 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using OnlineShop.Data;
 
 namespace OnlineShop.Areas.Admin.Controllers
 {
+    [Area("Admin")]
     public class SpecialTagsController : Controller
     {
+        private ApplicationDbContext _db;
+        public SpecialTagsController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
         public IActionResult Index()
         {
-            return View();
+            return View(_db.SpecialTags.ToList());
         }
     }
 }
